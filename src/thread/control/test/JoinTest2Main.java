6d0e1797e75f77182f91117c5eed72f0216a7945
taewoocode <qkrxodn6035@gmail.com>
2024-08-07 16:08:19 +0900

    동시실행

---
+++ src/thread/control/test/JoinTest2Main.java
@@ -0,0 +1,35 @@
+package thread.control.test;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class JoinTest2Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread( new MyTask(), "t1" );
+        Thread t2 = new Thread( new MyTask(), "t2" );
+        Thread t3 = new Thread( new MyTask(), "t3" );
+
+        //동시에 실행한다.
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println( "모든 스레드 실행 완료" ); //9초
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i <= 3; i++) {
+                log( i );
+                sleep( 1000 );
+            }
+        }
+    }
+}
+
+
