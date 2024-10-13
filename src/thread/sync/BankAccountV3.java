dd4c773c7f7ee0cd959d4dea35f683ac41059b68
taewoocode <qkrxodn6035@gmail.com>
2024-10-13 18:25:30 +0900

    thread

---
+++ src/thread/sync/BankAccountV3.java
@@ -0,0 +1,39 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV3 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV3(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log( "거래 시작: " + getClass().getSimpleName() );
+
+        synchronized (this) { //this = x001 lock
+            log( "[검증 시작] 출금액: " + amount + ", 잔액: " + balance );
+            if (balance < amount) {
+                log( "[검증 실패] 출금액: " + amount + ", 잔액: " + balance );
+                return false;
+            }
+            log( "[검증 완료] 출금액: " + amount + ", 잔액: " + balance );
+            sleep( 1000 ); //출금에 걸리는 시간
+            balance = balance - amount;
+
+            //잔고가 출금액 보다 많으면, 진행
+            log( "[검증 완료] 출금액: " + amount + ", 잔액: " + balance );
+        }
+        log( "거래 종료" );
+        return true;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return balance;
+    }
+}
--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -5,7 +5,8 @@ import static util.ThreadUtils.sleep;
 
 public class BankMain {
     public static void main(String[] args) throws InterruptedException {
-        BankAccount account = new BankAccountV1( 1000 );
+
+        BankAccount account = new BankAccountV2( 1000 );
 
         Thread t1 = new Thread( new WithdrawTask( account, 800 ), "t1" );
         Thread t2 = new Thread( new WithdrawTask( account, 800 ), "t2" );
